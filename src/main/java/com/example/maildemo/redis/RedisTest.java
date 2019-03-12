5811b4cae51dd48bfe830f8858b09429286c81d1
zhgxiaobei <310074230@qq,com>
2019-03-12 14:04:06 +0800

    集成redis集群

--- src/main/java/com/example/maildemo/redis/RedisTest.java
+++ src/main/java/com/example/maildemo/redis/RedisTest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("redis")
-public class test {
+public class RedisTest {
 
     @Autowired
     private RedisUtils redisUtils;
